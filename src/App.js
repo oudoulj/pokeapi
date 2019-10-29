823041cc0fb6d4857690b36284bc24bafffa0e5f
Jerome OUDOUL <jerome.oudoul@adevinta.com>
2019-10-29 16:52:52 +0100

    Class/State converted to hooks

--- src/App.js
+++ src/App.js
@@ -1,79 +1,109 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ky from "ky";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 import PokemonPageList from "./PokemonPageList";
 
-class App extends React.Component {
-  state = { pokemonList: [], pokemonDetails: [], page: 0 };
+const App = () => {
+  //state = { pokemonList: [], pokemonDetails: [], page: 0 };
+  const [pokemonList, setPokemonList] = useState([]);
+  const [pokemonDetails, setPokemonDetails] = useState([]);
+  const [page, setPage] = useState(1);
 
-  componentDidMount() {
-    this.loadPokemons();
-  }
+  // componentDidMount() {
+  //   this.loadPokemons();
+  // }
 
-  loadPokemons = () => {
-    (async () => {
-      const sleep = m => new Promise(r => setTimeout(r, m));
-      await sleep(1000);
-      const pokemonList = await ky
-        .get(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${this.state.page * 10}`)
-        .json();
-      this.setState({ pokemonList: pokemonList.results });
-      console.log(pokemonList.results);
-    })();
+  useEffect(() => {
+    loadPokemons();
+    // console.log(pokemonList);
+  }, []);
+
+  const loadPokemons = pageToLoad => {
+    // (async () => {
+    // const sleep = m => new Promise(r => setTimeout(r, m));
+    // await sleep(1000);
+    ky.get(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${(pageToLoad - 1) * 10}`)
+      .json()
+      .then(pokemonData => {
+        setPokemonList(pokemonData.results);
+      });
+    //this.setState({ pokemonList: pokemonList.results });
+    // setPokemonList(pokemonList.results);
+    // console.log(pokemonList.results);
+    // })();
   };
 
-  loadDetails = id => {
-    (async () => {
-      this.setState({ pokemonDetails: [] }); //reset
-      console.log("selected id = " + id);
-      const sleep = m => new Promise(r => setTimeout(r, m));
-      await sleep(1000);
-      const pokemonDetails = await ky.get(`https://pokeapi.co/api/v2/pokemon/${id}`).json();
-      this.setState({ pokemonDetails: pokemonDetails.abilities });
-      this.setState({ message: "" }); //reset
-      console.log(pokemonDetails.abilities);
-    })();
+  const loadDetails = id => {
+    // (async () => {
+    // this.setState({ pokemonDetails: [] }); //reset
+
+    console.log("selected id = " + id);
+    // const sleep = m => new Promise(r => setTimeout(r, m));
+    // await sleep(1000);
+    ky.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .json()
+      .then(pokeDetails => setPokemonDetails(pokeDetails.abilities));
+    // this.setState({ pokemonDetails: pokemonDetails.abilities });
+    // this.setState({ message: "" }); //reset
+    // setPokemonList(pokemonDetails.abilities);
+
+    // console.log(pokemonDetails.abilities);
+    // })();
   };
 
-  render() {
-    return (
-      <div className="some-page-wrapper">
-        <div className="row">
-          <div className="column">
-            My Pokemon List
-            <PokemonPageList
-              pageId={this.state.pageId}
-              pokemonList={this.state.pokemonList}
-              handleClick={this.loadPokemons}
-            />
+  return (
+    <div className="some-page-wrapper">
+      <div className="row">
+        <div className="column">
+          My Pokemon List
+          <PokemonPageList
+            //pageId={this.state.pageId}
+            pokemonList={pokemonList}
+            handleClick={loadDetails}
+          />
+          {page > 1 ? (
             <button
               onClick={() => {
-                this.setState({ page: this.state.page + 1 });
-                this.loadPokemons();
+                //this.setState({ page: this.state.page + 1 });
+                const nextPage = page - 1;
+                setPage(nextPage); //not reliable, maybe useless
+                console.log("nextPage", nextPage);
+                loadPokemons(nextPage);
               }}
             >
-              Next >>
+              {"<< Prev"}
             </button>
-            <Router>
-              <Link to={"/page/" + this.state.page}>Next ></Link>
-              {/* <Route path="/page/:pageId" component={} /> */}
-            </Router>
-          </div>
-          <div className="column">
-            Abilities for {this.state.pokemonName}
-            <br />
-            <ul>
-              {this.state.message}
-              {this.state.pokemonDetails.map((a, index) => (
-                <li key={index}>{a.ability.name}</li>
-              ))}
-            </ul>
-          </div>
+          ) : null}
+          <button
+            onClick={() => {
+              //this.setState({ page: this.state.page + 1 });
+              const nextPage = page + 1;
+              setPage(nextPage); //not reliable, maybe useless
+              console.log("nextPage", nextPage);
+              loadPokemons(nextPage);
+            }}
+          >
+            Next >>
+          </button>
+          <Router>
+            <Link to={"/page/" + page}>Next ></Link>
+            {/* <Route path="/page/:pageId" component={} /> */}
+          </Router>
+        </div>
+        <div className="column">
+          Abilities for {/* {pokemonName} */}
+          <br />
+          <ul>
+            {/* {message} */}
+            {pokemonDetails.map((a, index) => (
+              <li key={index}>{a.ability.name}</li>
+            ))}
+          </ul>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- src/PokemonPageList.js
+++ src/PokemonPageList.js
@@ -1,42 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-class PokemonPageList extends React.Component {
-  state = {
-    pokemonName: "",
-    message: ""
-  };
-  render() {
-    const { pokemonList, pageId, handleClick } = this.props;
-    return (
-      <div style={{ background: "lightblue" }}>
-        <ul>
-          {pokemonList.length === 0
-            ? "Loading..."
-            : pokemonList.map((p, index) => (
-                <li
-                  key={index}
-                  style={{ cursor: "pointer" }}
-                  onClick={() => {
-                    handleClick(pageId);
-                  }}
-                  //   onClick={() => {
-                  //     this.setState({ message: "chargement des détails..." });
-                  //     this.setState({ pokemonName: p.name });
-                  //     this.loadDetails(
-                  //       p.url
-                  //         .slice(0, -1)
-                  //         .split("/")
-                  //         .pop()
-                  //     );
-                  //   }}
-                >
-                  {p.name}
-                </li>
-              ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const PokemonPageList = ({ pokemonList, pageId, handleClick }) => {
+  //   state = {
+  //     pokemonName: "",
+  //     message: ""
+  //   };
+
+  const [pokemonName, setPokemonName] = useState("");
+  const [message, setMessage] = useState("");
+
+  //   const { pokemonList, pageId, handleClick } = this.props;
+  return (
+    <div style={{ background: "lightblue" }}>
+      <ul>
+        {pokemonList.length === 0
+          ? "Loading..."
+          : pokemonList.map((p, index) => (
+              <li
+                key={index}
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  console.log("onClick on li element", index);
+                  handleClick(pageId);
+                }}
+                //   onClick={() => {
+                //     this.setState({ message: "chargement des détails..." });
+                //     this.setState({ pokemonName: p.name });
+                //     this.loadDetails(
+                //       p.url
+                //         .slice(0, -1)
+                //         .split("/")
+                //         .pop()
+                //     );
+                //   }}
+              >
+                {p.name}
+              </li>
+            ))}
+      </ul>
+    </div>
+  );
+};
 
 export default PokemonPageList;
